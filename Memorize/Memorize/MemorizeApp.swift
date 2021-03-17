0fbbe2409fc811b399a313fcadbcca2eba5e0b28
Elias Gabriel <elias.gabrielfn12@gmail.com>
2021-03-16 20:16:16 -0400

    feat: add project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Elias Gabriel dos Santos Correa on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
